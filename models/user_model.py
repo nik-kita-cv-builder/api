87f909ac742c1e649d4e432441fcec609e694875
nik-kita <nik-kita.deno.dev>
2024-04-06 17:01:37 +0200

    update User model with ondelete-delete children

--- models/user_model.py
+++ models/user_model.py
@@ -1,9 +1,22 @@
 from sqlmodel import Relationship
 from sqlalchemy import orm
 
+from models.avatar_model import Avatar
+from models.contact_model import Contact
+from models.education_model import Education
+from models.experience_model import Experience
+from models.language_model import Language
+from models.skill_model import Skill
+
 from .profile_model import Profile
 from .auth_provider_enum import AuthProvider, AuthProviderEnum
 from sqlmodel import Field, SQLModel
+
+relation_ondelete_delete = Relationship(
+    sa_relationship=orm.relationship(
+        Profile,
+        cascade='all, delete-orphan',
+    ))
 
 
 class User(SQLModel, table=True):
@@ -14,8 +27,11 @@ class User(SQLModel, table=True):
     sub: str | None = Field(str, unique=True)
     auth: AuthProviderEnum = Field(AuthProvider)
 
-    profiles: list[Profile] = Relationship(
-        sa_relationship=orm.relationship(
-            Profile,
-            cascade='all, delete-orphan',
-        ))
+    profiles: list[Profile] = relation_ondelete_delete
+    avatars: list[Avatar] = relation_ondelete_delete
+
+    contacts: list[Contact] = relation_ondelete_delete
+    skills: list[Skill] = relation_ondelete_delete
+    educations: list[Education] = relation_ondelete_delete
+    experiences: list[Experience] = relation_ondelete_delete
+    languages: list[Language] = relation_ondelete_delete
